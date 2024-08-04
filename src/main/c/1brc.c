f157e19ef71dc0722269c0483769a0451dece765
Julio Vidaurre <julio.vidaurre@gmail.com>
2024-08-04 14:05:01 -0400

    Allocate all possible entries at the beginning

--- src/main/c/1brc.c
+++ src/main/c/1brc.c
@@ -65,10 +65,100 @@ static char *next_new_line(char *ptr)
     return next_character(ptr, '\n');
 }
 
+#define MAX_STATIONS 10000
+
+typedef struct HashMapEntry {
+    // u64 key;
+    char *station_name;
+    int station_name_length;
+    s16 value;
+    struct HashMapEntry *next;
+} HashMapEntry;
+
+typedef struct HashMap {
+    HashMapEntry *entries[MAX_STATIONS];
+} HashMap;
+
+static HashMap *hash_map_init()
+{
+    HashMap *hash_map = (HashMap *)malloc(sizeof(HashMap));
+    for (int i = 0; i < MAX_STATIONS; ++i) {
+        hash_map->entries[i] = NULL;
+    }
+    
+    return hash_map;
+}
+
+// static int hash(u64 h)
+// {
+//     h ^= (h >> 20) ^ (h >> 12);
+//     return (int)((h ^ (h >> 7) ^ (h >> 4)) % MAX_STATIONS);
+// }
+static int hash(char *string, int length)
+{
+    int h = 0;
+    while (--length >= 0) {
+        char c = string[length];
+        h ^= (c >> 20) ^ (c >> 12);
+        h ^= (c >> 7) ^ (c >> 4);
+    }
+    
+    return h % MAX_STATIONS;
+}
+
+static int str_equals(char *s1, int length1, char *s2, int length2)
+{
+    if (length1 != length2) return 0;
+    
+    while (--length1) {
+        if (*s1++ != *s2++) return 0;
+    }
+    
+    return 1;
+}
+
+static void hash_map_insert(HashMap *hash_map, HashMapEntry *new_entry)
+{
+    int entry_hash = hash(new_entry->station_name, new_entry->station_name_length);
+    HashMapEntry *entry = hash_map->entries[entry_hash];
+    while (entry)
+    {
+        if (str_equals(entry->station_name, entry->station_name_length, 
+                       new_entry->station_name, new_entry->station_name_length))
+        {
+            return;
+        }
+        
+        entry = entry->next;
+    }
+    
+    new_entry->next = hash_map->entries[entry_hash];
+    hash_map->entries[entry_hash] = new_entry;
+}
+
+static HashMapEntry *hash_map_get(HashMap *hash_map, char *station_name, int station_name_length)
+{
+    int entry_hash = hash(station_name, station_name_length);
+    HashMapEntry *entry = hash_map->entries[entry_hash];
+    while (entry && !str_equals(entry->station_name, entry->station_name_length,
+                                station_name, station_name_length))
+    {
+        entry = entry->next;
+    }
+    
+    return entry;
+}
+
 typedef struct ThreadParams {
+    u8 tid;
     char *file_ptr_start;
     char *file_ptr_end;
+    int entries_index;
+    // HashMap *hash_map;
 } ThreadParams;
+
+static HashMap *hash_map;
+static HashMapEntry *all_entries;
 
 DWORD thread_function(LPVOID lp_param)
 {
@@ -108,8 +198,23 @@ DWORD thread_function(LPVOID lp_param)
                 }
             }
             
+            // TODO: Allocate the entire hash_map size entries at the beginning
+            // Maybe allocating a large amount of memory at the beginning (or using a global partitioned hashmap).
+            // Like: 0...9 entries belongs to thread 0's entries; 10...19 belongs to thread 1's entries; and so on...
+            HashMapEntry *entry = hash_map_get(hash_map, ptr, station_name_length);
+            if (!entry) {
+                int entries_base_index = params->tid * MAX_STATIONS;
+                // entry = (HashMapEntry *)malloc(sizeof(HashMapEntry));
+                entry = all_entries + entries_base_index + params->entries_index++;
+                entry->station_name = ptr;
+                entry->station_name_length = station_name_length;
+                entry->value = value;
+                hash_map_insert(hash_map, entry);
+            }
             
-            printf("station name = %.*ls, value = %.*s\n", station_name_length, station_name, value_length, ptr_semicolon + 1);
+            // TODO: compute min, max, sum
+            
+            // printf("station name = %.*ls, value = %.*s\n", station_name_length, station_name, value_length, ptr_semicolon + 1);
         }
         
         ptr = ptr_new_line + 1;
@@ -132,8 +237,13 @@ int main(int argc, char **argv)
     
     LARGE_INTEGER start = win32_get_wall_clock();
 
-    // char *filename = "../measurements.txt";
+#if 1
+    int n_threads = system_info.dwNumberOfProcessors;
+    char *filename = "../measurements.txt";
+#else
+    int n_threads = 1;
     char *filename = "../measurements_test.txt";
+#endif
     HANDLE file_handle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
     if (file_handle == INVALID_HANDLE_VALUE) {
         fprintf(stderr, "ERROR: Cannot open file %s, error code = %d\n", filename, GetLastError());
@@ -159,17 +269,24 @@ int main(int argc, char **argv)
 
     setlocale(LC_ALL, "en_US.UTF8");
         
-    // int n_threads = system_info.dwNumberOfProcessors;
-    int n_threads = 1;
     HANDLE *threads = (HANDLE *)malloc(n_threads * sizeof(HANDLE));
     ThreadParams *thread_params = (ThreadParams *)malloc(n_threads * sizeof(ThreadParams));
+    
+    hash_map = hash_map_init();
+    all_entries = (HashMapEntry *)malloc(sizeof(HashMapEntry) * MAX_STATIONS * n_threads);
+    // for (int i = 0; i < MAX_STATIONS * n_threads; ++i) {
+    //     all_entries[i] = ;
+    // }
     
     s64 chunk_size = file_size / n_threads;
     char *file_end_address = ((char *)file) + file_size;
     char *file_ptr = (char *)file;
     for (int i = 0; i < n_threads; i++) {
+        thread_params[i].tid = (u8)i;
         thread_params[i].file_ptr_start = file_ptr;
         thread_params[i].file_ptr_end = (i == n_threads - 1) ? file_end_address : next_new_line(file_ptr + chunk_size);
+        thread_params[i].entries_index = 0;
+        // thread_params[i].hash_map = hash_map_init();
         
         file_ptr = thread_params[i].file_ptr_end + 1;
         
@@ -184,6 +301,10 @@ int main(int argc, char **argv)
     // }
 
     // printf("lines = %d\n", lines);
+    // for (int i = 0; i < n_threads; i++) {
+    //     HashMap *hash_map = thread_params[i].hash_map;
+    //     int x = 5;
+    // }
 
     LARGE_INTEGER end = win32_get_wall_clock();
     
